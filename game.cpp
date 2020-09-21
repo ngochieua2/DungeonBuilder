b9144248dd63246891181096fc6d8ad0a45b419f
Ngoc Hieu Nguyen <nguny071@mymail.unisa.edu.au>
2020-09-21 18:20:05 +0930

    test example map

--- game.cpp
+++ game.cpp
@@ -26,28 +26,28 @@ void Game::createExampleLevel(){
     for(int i = 0; i < dungeon->numberOfRooms(); ++i){
          dungeon->addRoom(_builder->buildRoom(i+1));
     }
-    //Build an open way door in the East of room 1 and an open way door in the West of room 2
+    //Build an open doorway in the East of room 1 to the West of room 2
     _builder->builDoorway(dungeon->retrieveRoom(1),dungeon->retrieveRoom(2),
                           Room::Direction::East,static_cast<DungeonLevelBuilder::MoveConstraints>(0));
-    //Build a block door way in the East of room 2 and a block door way in the West of room 3
+    //Build a block doorway in the East of room 2 and a block doorway in the West of room 3
     _builder->builDoorway(dungeon->retrieveRoom(2),dungeon->retrieveRoom(3),
                           Room::Direction::East,static_cast<DungeonLevelBuilder::MoveConstraints>(3));
-    //Build an one way door in the South of room 1 and an one way door in the North of room 4
+    //Build an one way door in the South of room 1 to the North of room 4
     _builder->builDoorway(dungeon->retrieveRoom(1),dungeon->retrieveRoom(4),
                           Room::Direction::South,static_cast<DungeonLevelBuilder::MoveConstraints>(2));
-    //Build an open-way door in the South of room 2 and an open door way in the North of room 5
+    //Build an open doorway in the South of room 2 to the North of room 5
     _builder->builDoorway(dungeon->retrieveRoom(2),dungeon->retrieveRoom(5),
                           Room::Direction::South,static_cast<DungeonLevelBuilder::MoveConstraints>(0));
-    //Build an open-way door in the South of room 3 and locked door way in the North of room 6
+    //Build an one way door in the South of room 3 and locked door way in the North of room 6
     _builder->builDoorway(dungeon->retrieveRoom(3),dungeon->retrieveRoom(6),
                           Room::Direction::South,static_cast<DungeonLevelBuilder::MoveConstraints>(8));
     //Build an one way door in the East of room 4 to the West of room 5
     _builder->builDoorway(dungeon->retrieveRoom(4),dungeon->retrieveRoom(5),
                           Room::Direction::East,static_cast<DungeonLevelBuilder::MoveConstraints>(2));
-    //Build an open door way in the East of room 5 to the West of room 6
+    //Build an open doorway in the East of room 5 to the West of room 6
     _builder->builDoorway(dungeon->retrieveRoom(5),dungeon->retrieveRoom(6),
                           Room::Direction::East,static_cast<DungeonLevelBuilder::MoveConstraints>(0));
-    //Build a blocked door way in the South of room 4 and the North of room 7
+    //Build a blocked doorway in the South of room 4 and the North of room 7
     _builder->builDoorway(dungeon->retrieveRoom(4),dungeon->retrieveRoom(7),
                           Room::Direction::South,static_cast<DungeonLevelBuilder::MoveConstraints>(3));
     //Build an open way door in the South of room 5 and the North of room 8
@@ -65,8 +65,6 @@ void Game::createExampleLevel(){
     _builder->buildEntrance(dungeon->retrieveRoom(1),Room::Direction::North);
     //Build exit in the East of room 9
     _builder->buildExit(dungeon->retrieveRoom(9), Room::Direction::East);
-    ;
-
 
 
 }
